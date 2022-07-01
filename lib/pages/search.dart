1ad69e62c99e82a7c12997dfbc9642283d6f14f3
Jeremia M <39850621+jeremia49@users.noreply.github.com>
2022-07-01 12:31:47 +0700

    implement search and filter

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,8 +1,9 @@
-import 'package:bestbrand/dummy_data.dart';
 import 'package:flutter/material.dart';
 import 'package:bestbrand/components/product_view.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../controllers/product_controller.dart';
 import '../models/product.dart';
@@ -19,6 +20,9 @@ class SearchPage extends StatefulWidget {
 class _SearchPageState extends State<SearchPage> {
   late List<Product> result;
   String searchquery = '';
+  String? filter;
+  String? sortcategory;
+  bool? sort;
 
   List<Product> searchq(List<Product> productList, String searchquery) {
     final query = searchquery.toLowerCase();
@@ -28,11 +32,40 @@ class _SearchPageState extends State<SearchPage> {
     }).toList();
   }
 
+  int getPrice(Product product) {
+    if (product.isPromo) {
+      return product.hargaPromo;
+    }
+    return product.harga;
+  }
+
   @override
   Widget build(BuildContext context) {
     final ProductController productController = Get.find();
     if (searchquery.isEmpty) {
-      result = productController.productList;
+      result = List.of(productController.productList);
+    }
+    if (filter != null) {
+      if (filter == 'diskon') {
+        result = result.where((e) => e.isPromo == true).toList();
+      } else {
+        result = result.where((e) => e.tag.contains(filter)).toList();
+      }
+    }
+    if (sort != null && sortcategory != null) {
+      if (sortcategory == 'name') {
+        if (sort == true) {
+          result.sort((a, b) => a.name.compareTo(b.name));
+        } else {
+          result.sort((a, b) => b.name.compareTo(a.name));
+        }
+      } else if (sortcategory == 'harga') {
+        if (sort == true) {
+          result.sort((a, b) => getPrice(a).compareTo(getPrice(b)));
+        } else {
+          result.sort((a, b) => getPrice(b).compareTo(getPrice(a)));
+        }
+      }
     }
 
     return Column(
@@ -41,13 +74,21 @@ class _SearchPageState extends State<SearchPage> {
       mainAxisSize: MainAxisSize.max,
       children: [
         TextField(
-          decoration: const InputDecoration(
+          decoration: InputDecoration(
               prefixIcon: Icon(
                 Icons.search,
                 color: Colors.black,
+                size: 20.sp,
               ),
-              border: OutlineInputBorder(),
+              border: const OutlineInputBorder(),
               hintText: 'Ketik produk yang anda cari ...'),
+          style: GoogleFonts.montserrat(
+            textStyle: TextStyle(
+              color: Colors.black,
+              fontSize: 15.sp,
+            ),
+            fontWeight: FontWeight.normal,
+          ),
           onChanged: ((e) {
             setState(
               () {
@@ -66,13 +107,247 @@ class _SearchPageState extends State<SearchPage> {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     TextButton.icon(
-                        onPressed: null,
-                        icon: const Icon(Icons.filter_alt_outlined),
-                        label: const Text('Filter')),
+                      onPressed: () {
+                        showModalBottomSheet(
+                          context: context,
+                          shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.vertical(
+                            top: Radius.circular(25),
+                          )),
+                          builder: (context) => Container(
+                            width: double.infinity,
+                            padding: EdgeInsets.all(20),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  'Filter berdasarkan :',
+                                  style: GoogleFonts.montserrat(
+                                    textStyle: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 18.sp,
+                                    ),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 10.h,
+                                ),
+                                Wrap(
+                                  children: [
+                                    FilterChip(
+                                      selected: filter == 'diskon',
+                                      label: Text(
+                                        'Diskon',
+                                        style: GoogleFonts.montserrat(
+                                          textStyle: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 15.sp,
+                                          ),
+                                          fontWeight: FontWeight.normal,
+                                        ),
+                                      ),
+                                      onSelected: (bool value) {
+                                        setState(() {
+                                          if (filter == 'diskon') {
+                                            filter = null;
+                                          } else {
+                                            filter = 'diskon';
+                                          }
+                                        });
+                                        Navigator.of(context).pop();
+                                      },
+                                    )
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                      icon: Icon(
+                        Icons.filter_alt_outlined,
+                        size: 17.sp,
+                        color: Colors.black,
+                      ),
+                      label: Text(
+                        'Filter',
+                        style: GoogleFonts.montserrat(
+                          textStyle: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15.sp,
+                          ),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
                     TextButton.icon(
-                        onPressed: null,
-                        icon: const Icon(Icons.sort_by_alpha_outlined),
-                        label: const Text('Sort')),
+                      onPressed: () {
+                        showModalBottomSheet(
+                          context: context,
+                          shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.vertical(
+                            top: Radius.circular(25),
+                          )),
+                          builder: (context) => Container(
+                            width: double.infinity,
+                            padding: EdgeInsets.all(20),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  'Sorting berdasarkan :',
+                                  style: GoogleFonts.montserrat(
+                                    textStyle: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 18.sp,
+                                    ),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 10.h,
+                                ),
+                                Wrap(
+                                  children: [
+                                    FilterChip(
+                                      selected: sortcategory == 'name',
+                                      label: Text(
+                                        'Nama',
+                                        style: GoogleFonts.montserrat(
+                                          textStyle: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 15.sp,
+                                          ),
+                                          fontWeight: FontWeight.normal,
+                                        ),
+                                      ),
+                                      onSelected: (bool value) {
+                                        setState(() {
+                                          if (sortcategory == 'name') {
+                                            sortcategory = null;
+                                          } else {
+                                            sortcategory = 'name';
+                                          }
+                                        });
+                                        Navigator.of(context).pop();
+                                      },
+                                    ),
+                                    SizedBox(
+                                      width: 10.w,
+                                    ),
+                                    FilterChip(
+                                      selected: sortcategory == 'harga',
+                                      label: Text(
+                                        'Harga',
+                                        style: GoogleFonts.montserrat(
+                                          textStyle: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 15.sp,
+                                          ),
+                                          fontWeight: FontWeight.normal,
+                                        ),
+                                      ),
+                                      onSelected: (bool value) {
+                                        setState(() {
+                                          if (sortcategory == 'harga') {
+                                            sortcategory = null;
+                                          } else {
+                                            sortcategory = 'harga';
+                                          }
+                                        });
+                                        Navigator.of(context).pop();
+                                      },
+                                    )
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: 10.h,
+                                ),
+                                Text(
+                                  'Secara :',
+                                  style: GoogleFonts.montserrat(
+                                    textStyle: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 18.sp,
+                                    ),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Wrap(
+                                  children: [
+                                    FilterChip(
+                                      selected: sort == true,
+                                      label: Text(
+                                        'Menurun',
+                                        style: GoogleFonts.montserrat(
+                                          textStyle: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 15.sp,
+                                          ),
+                                          fontWeight: FontWeight.normal,
+                                        ),
+                                      ),
+                                      onSelected: (bool value) {
+                                        setState(() {
+                                          if (sort == true) {
+                                            sort = null;
+                                          } else {
+                                            sort = true;
+                                          }
+                                        });
+                                        Navigator.of(context).pop();
+                                      },
+                                    ),
+                                    SizedBox(
+                                      width: 10.h,
+                                    ),
+                                    FilterChip(
+                                      selected: sort == false,
+                                      label: Text(
+                                        'Menaik',
+                                        style: GoogleFonts.montserrat(
+                                          textStyle: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 15.sp,
+                                          ),
+                                          fontWeight: FontWeight.normal,
+                                        ),
+                                      ),
+                                      onSelected: (bool value) {
+                                        setState(() {
+                                          if (sort == false) {
+                                            sort = null;
+                                          } else {
+                                            sort = false;
+                                          }
+                                        });
+                                        Navigator.of(context).pop();
+                                      },
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                      icon: Icon(
+                        Icons.sort_by_alpha_outlined,
+                        size: 18.sp,
+                        color: Colors.black,
+                      ),
+                      label: Text(
+                        'Sort',
+                        style: GoogleFonts.montserrat(
+                          textStyle: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15.sp,
+                          ),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
                   ],
                 ),
                 Expanded(
